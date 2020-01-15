e2fa087fc04409c93edf204a3792c428004294b2
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-15 04:24:43 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,7 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     self.where("rating = ?", self.highest_rating).first
+    self.where("rating = ?", self.maximum(:rating)).first
   end
 
   def self.lowest_rating
