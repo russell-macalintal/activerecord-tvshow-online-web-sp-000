7bc49c3d12585834e9e182f2b9b0dd0cd76ca46b
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-15 04:06:30 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,4 +1,4 @@
-class AddSeasonToShows < ActiveRecord::Migration
+class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
     # add_column :shows, :season, :string
     change_table :shows do |t|
