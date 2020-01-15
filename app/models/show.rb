74f6e49d26c21aea103747b501ce59e020d0641e
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-15 04:18:52 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,11 +8,11 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
-
+    self.minimum(:rating)
   end
 
   def self.least_popular_show
-
+    self.where("rating = ?", self.lowest_rating).first
   end
 
   def self.ratings_sum
